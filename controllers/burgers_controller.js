84aa09adf4375b8e1c87474d029a6b9c3f7e3a9d
Taylor <tcuneo5@gmail.com>
2019-01-22 20:20:40 -0500

    trial and error

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,7 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/index", function(req, res) {
+router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
       burgers: data
@@ -16,7 +16,7 @@ router.get("/index", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
+router.post("/", function(req, res) {
   burger.create([
     "name"
   ], [
